652173d0c98d2ca6ea68183dbe162cde18a140fc
hajalibayram <hajalibayram@outlook.com>
2025-05-09 16:07:39 +0200

    GoiEner dataset first commit:  - command added  - split, preprocess implemented (missing download)  - TODO: pydoc, original dataset preprocessing

--- app/app.py
+++ app/app.py
@@ -4,7 +4,8 @@ import typer
 from rich.logging import RichHandler
 from typing_extensions import Annotated
 
-from opensynth.datasets.low_carbon_london import get_data
+from opensynth.datasets.low_carbon_london import get_data as get_data_lcl
+from src.opensynth.datasets.goiener import get_data as get_data_goiener
 
 logging.basicConfig(
     level=logging.INFO,
@@ -18,7 +19,7 @@ logger = logging.getLogger(__name__)
 app = typer.Typer(context_settings=dict(max_content_width=800))
 
 
-@app.command()
+@app.command("get_lcl_data")
 def get_lcl_data(
     download: Annotated[
         bool, typer.Option("--download", help="Downloads LCL data.")
@@ -41,7 +42,34 @@ def get_lcl_data(
     """
     Download, split and preprocess the Low Carbon London dataset.
     """
-    get_data.get_lcl_data(
+    get_data_lcl.get_lcl_data(
+        download=download, split=split, preprocess=preprocess
+    )
+
+@app.command("get_goiener_data")
+def get_goiener_data(
+    download: Annotated[
+        bool, typer.Option("--download", help="Downloads GoiEner data.")
+    ] = False,
+    split: Annotated[
+        bool,
+        typer.Option(
+            "--split", help="Splits GoiEner households into training/ holdout set"
+        ),
+    ] = False,
+    preprocess: Annotated[
+        bool,
+        typer.Option(
+            "--preprocess",
+            help="Preprocesses GoiEner data to create 48-half hour daily"
+            "load profiles",
+        ),
+    ] = False,
+):
+    """
+    Download, split and preprocess the Low Carbon London dataset.
+    """
+    get_data_goiener.get_goiener_data(
         download=download, split=split, preprocess=preprocess
     )
 
